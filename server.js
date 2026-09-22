91a4e392f6215cda2154554d4e25481b9b01176b
abuyzzn <abuyzzn@gmail.com>
2026-09-22 22:07:08 +0300

    Fix: Use dynamic PORT from environment variable
    Fix: Use dynamic PORT from environment variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
